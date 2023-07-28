c0bbf65bf5af09d310f5b998bfe2666e6667475b
Naveen <naveensenthil125@gmail.com>
2023-07-28 12:17:25 +0530

    Age in Months,weeks,days changes

--- app/src/main/java/com/example/agetomincalc/MainActivity.kt
+++ app/src/main/java/com/example/agetomincalc/MainActivity.kt
@@ -32,6 +32,8 @@ class MainActivity : AppCompatActivity() {
         val tvSelectedDateInMinutes: TextView = findViewById(R.id.tvSelectedDateInMinutes)
         val editTextText: EditText = findViewById(R.id.editTextText)
         val displayName: TextView = findViewById(R.id.displayName)
+        val inDays: TextView = findViewById(R.id.inDays)
+        val inWeeks: TextView = findViewById(R.id.inWeeks)
 
 
 
@@ -46,12 +48,36 @@ class MainActivity : AppCompatActivity() {
                 val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
                 val theDate = sdf.parse(selectedDate)
                 theDate?.let {
-                    val selectedDateInMinutes = theDate.time / 60000
+//                    val selectedDateInMinutes = theDate.time / 60000
                     val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
                     currentDate?.let {
-                        val currentDateToMinutes = currentDate.time / 60000
-                        val differenceInMinutes = currentDateToMinutes - selectedDateInMinutes
-                        tvSelectedDateInMinutes.text = differenceInMinutes.toString()
+                        var different:Long  = currentDate.time - theDate.time ;
+                        var forWeek: Long = different
+                        var forTotalDays: Long = different
+                        val secondsInMilli: Long  = 1000;
+                        val minutesInMilli: Long  = secondsInMilli * 60;
+                        val hoursInMilli: Long  = minutesInMilli * 60;
+                        val daysInMilli: Long = hoursInMilli * 24;
+                        val weekInMilli: Long = daysInMilli * 7
+                        val monthsInMilli: Long = daysInMilli * 30;
+
+                        val elapsedMonths: Long = different / monthsInMilli;
+                        different = different % monthsInMilli
+                        val diffMonth: Long = elapsedMonths
+
+                        val elapsedWeeks: Long = forWeek / weekInMilli
+                        forWeek = forWeek % weekInMilli
+                        val forWeekElapsedDay: Long = forWeek / daysInMilli
+
+                        val elapsedDays: Long = different / daysInMilli;
+                        different = different % daysInMilli;
+                        val diffDays: Long = elapsedDays;
+
+                        val totalDays: Long = forTotalDays / daysInMilli
+
+                        tvSelectedDateInMinutes.text = "$diffMonth Months And $diffDays Days or"
+                        inWeeks.text = "$elapsedWeeks week and $forWeekElapsedDay days Old"
+                        inDays.text = "$totalDays days or"
                     }
                 }
 
